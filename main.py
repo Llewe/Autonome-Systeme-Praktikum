cf633e938164eb6beda34f675f7ab6d02d11ef15
Llewe <16879918+Llewe@users.noreply.github.com>
2022-06-14 11:35:35 +0200

    added src and out folder

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-
-
 from codecarbon import OfflineEmissionsTracker
 
-
-print("hi")
-
-
-tracker = OfflineEmissionsTracker(country_iso_code="DEU")
+tracker = OfflineEmissionsTracker(output_dir="./out/", country_iso_code="DEU")
 tracker.start()
 # training
 print("hi")
