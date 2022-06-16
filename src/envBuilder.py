9ef71c49cc0aedc60e51ccab2bd1fd22d91707a9
Llewe <16879918+Llewe@users.noreply.github.com>
2022-06-16 22:25:41 +0200

    added  parameter randomisation & unity-env for ellipsoid,bouncy ellipsoid

--- src/envBuilder.py
+++ src/envBuilder.py
@@ -1,7 +1,10 @@
+from random import Random, random
 from mlagents_envs.environment import UnityEnvironment
+from mlagents_envs.side_channel.environment_parameters_channel import EnvironmentParametersChannel
 from gym_unity.envs import UnityToGymWrapper
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
 import gym
+import sys
 
 import os
 
@@ -14,7 +17,7 @@ def createGymEnv(name='MountainCarContinuous-v0'):
 """
 create a unity domain
 """
-def createUnityEnv(name='3DBall1',no_graphics=True,time_scale=20.):
+def createUnityEnv(name='3DEllipsoid1-Bouncy',no_graphics=True,time_scale=20.,rngMass=False,rngGravity=False,rngScale=False,individualScale=False):
   rootDir = os.getcwd()
   unityEnvDir = os.path.join(rootDir, "unity-env",name)
   if not os.path.exists(unityEnvDir):
@@ -23,13 +26,33 @@ def createUnityEnv(name='3DBall1',no_graphics=True,time_scale=20.):
   
   unityExe = os.path.abspath(os.path.join(unityEnvDir, "asp"))
   
+  envChannel = EnvironmentParametersChannel()
+  
   if(no_graphics):
     channel = EngineConfigurationChannel()
+    envUnity = UnityEnvironment(file_name=unityExe,no_graphics=no_graphics, side_channels=[channel,envChannel])
     channel.set_configuration_parameters(time_scale = time_scale)
-    envUnity = UnityEnvironment(file_name=unityExe,no_graphics=no_graphics, side_channels=[channel])
   else:
-    envUnity = UnityEnvironment(file_name=unityExe,no_graphics=no_graphics, side_channels=[])
-
+    envUnity = UnityEnvironment(file_name=unityExe,no_graphics=no_graphics, side_channels=[envChannel])
+    
+  # Recommended parameter bounds: https://unity-technologies.github.io/ml-agents/Learning-Environment-Examples/#3dball-3d-balance-ball
+  
+  
+  if rngMass:
+    envChannel.set_gaussian_sampler_parameters("mass", 0.1,20,12)
+  
+  if rngGravity:
+    envChannel.set_uniform_sampler_parameters("gravity", 4,105,123124)
+  
+  if rngScale:
+    if individualScale:
+      envChannel.set_uniform_sampler_parameters("scale_x", 0.2,5,31512)
+      envChannel.set_uniform_sampler_parameters("scale_y",0.2,5,1231526)
+      envChannel.set_uniform_sampler_parameters("scale_z",0.2,5,344684)
+    else:
+      envChannel.set_uniform_sampler_parameters("scale", 0.2,5,5463343)
+    
   env = UnityToGymWrapper(envUnity)
 
   return env
+
