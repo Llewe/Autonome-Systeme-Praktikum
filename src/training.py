1f705cc918f3aa7332d54361460fcc2e3b4e131d
ilijana <spasojevic.ilijana@gmail.com>
2022-06-17 13:22:27 +0200

    Refactored: Params + deleted unused code

--- src/training.py
+++ src/training.py
@@ -40,40 +40,36 @@ def training(env,agent,episodes):
     
     
 if __name__ == '__main__':
-    env_name = "MountainCarContinuous-v0"
-    has_continuous_action_space = True  # continuous action space; else discrete
-    max_ep_len = 1000                   # max timesteps in one episode
-    max_training_timesteps = int(3e6)   # break training loop if timeteps > max_training_timesteps
-    print_freq = max_ep_len * 10        # print avg reward in the interval (in num timesteps)
-    action_std = 0.6                    # starting std for action distribution (Multivariate Normal)
-    action_std_decay_rate = 0.05        # linearly decay action_std (action_std = action_std - action_std_decay_rate)
-    min_action_std = 0.1                # minimum action_std (stop decay after action_std <= min_action_std)
-    action_std_decay_freq = int(2.5e5)  # action_std decay frequency (in num timesteps)
+    env_name = "MountainCarContinuous-v0"                
 
-    ################ PPO hyperparameters ################
-    update_timestep = max_ep_len * 4      # update policy every n timesteps
-    K_epochs = 80               # update policy for K epochs in one PPO update
-
-    eps_clip = 0.2          # clip parameter for PPO
-    gamma = 0.99            # discount factor
-
-    lr_actor = 0.0003       # learning rate for actor network
-    lr_critic = 0.001       # learning rate for critic network
+    params = {}
+    params["has_continuous_action_space"] = True
+    params["update_timestep"] = 4000
+    params["K_epochs"] = 80               
+    params["eps_clip"] = 0.2          
+    params["gamma"] = 0.99            
+    params["lr_actor"] = 0.0003       
+    params["lr_critic"] = 0.001  
+    params["action_std"] = 0.6     
 
     print("training environment name : " + env_name)
 
     env = gym.make(env_name)
-
-    # state space dimension
+    
     state_dim = env.observation_space.shape[0]
-
-    # action space dimension
-    if has_continuous_action_space:
+    if params["has_continuous_action_space"]:
         action_dim = env.action_space.shape[0]
     else:
         action_dim = env.action_space.n
         
-    # initialize a PPO agent
-    agent = PPO(state_dim, action_dim, lr_actor, lr_critic, gamma, K_epochs, eps_clip, has_continuous_action_space, action_std)
+    agent = PPO(state_dim, 
+                action_dim, 
+                params["lr_actor"], 
+                params["lr_critic"], 
+                params["gamma"], 
+                params["K_epochs"], 
+                params["eps_clip"], 
+                params["has_continuous_action_space"], 
+                params["action_std"])
 
-    training(env=env, agent=agent, episodes=K_epochs)
+    training(env=env, agent=agent, episodes=params["K_epochs"])
