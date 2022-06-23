836502ee41b1dde54c54257304c7a55030dec07b
KathiWinter <kathi.winter@t-online.de>
2022-06-23 12:25:07 +0200

    Intermediate Result

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from ast import arg
+import pickle
+from src.PPO import PPO
 from codecarbon import OfflineEmissionsTracker
 from src.trainingDemo import replayDemo,trainingDemo
-from src.envBuilder import buildFromArgs
+from src.envBuilder import buildFromArgs, createUnityEnv
 from src.training import PPOTraining
 import ray
-import pickle
+from ray.tune.registry import register_env
+import os
 import argparse
 
 def parseArguments():
@@ -37,26 +40,76 @@ args = parseArguments()
 
 modelName = args.model
 
-#env = buildFromArgs(args)
-
 if args.demo:
     if args.replay:
+        env = buildFromArgs(args)
         replayDemo(env,modelName,True)
     else:
+        env = buildFromArgs(args)
         trainingDemo(env,modelName,args.episodes)     
 else:
     if args.replay:
         print("Replay mode for non-demo not implemented yet")
     else:
+    
+        
         #Trainng mode
-        ray.init()
-        ppoTraining = PPOTraining.remote(args)
-     
+        
+    
+        
+            # create directory and file to save checkpoint to
+        directory = "PPO_preTrained"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
-        asdf = ppoTraining.startTraining.remote(3)
-        #xid = ray.put(asdf)
-        xnew = ray.get(asdf)
-        s = pickle.dumps(xnew)
-        t = pickle.loads(s)
+        directory = directory + '/'
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        checkpoint_path = os.path.join(directory, 'net_{}_{}'.format('logs', 0))
+        
+        #env = buildFromArgs(args)
+        register_env("unity_3DBall", lambda args: createUnityEnv(args))
+     
+        
+        state_dim = 8#env.observation_space[0].shape[0]
+        
+        action_dim = 2# env.action_space.shape[0]
+       
+        # create PPO driven agent with hyperparameters
+        agent = PPO(state_dim, 
+            action_dim, 
+            args.lr_actor, 
+            args.lr_critic, 
+            args.gamma, 
+            args.k_epochs, 
+            args.epsilon_clip, 
+            args.action_std
+            )
+      
+        
+        config = {
+            "nr_episodes": 100,
+            "render": True,
+            "update_timestep": 64,
+            "K_epochs": 10,
+            "eps_clip": 0.2,
+            "gamma": 0.99,
+            "lr_actor": 0.0003,
+            "lr_critic": 0.001,
+            "action_std_decay_rate": 0.05,
+            "min_action_std": 0.1,
+            "action_std_decay_freq": int(2.5e5),
+            "save_model_freq": int(1e1)
+        }
+
+        ray.init()
+        ppoTraining = PPOTraining.remote(env = "unity_3DBall", checkpoint_path=checkpoint_path, agent = agent)
+        
+        results = ray.get(ppoTraining.startTraining.remote(config))
+        print("results=",results)
+
+        if ray.is_initialized():
+            ray.shutdown()
             
 #tracker.stop()
--- src/training.py
+++ src/training.py
@@ -1,31 +1,32 @@
+import json
 import os
 import numpy as np
 import gym
 from src.PPO import PPO
 import ray
-from ray import tune
+from ray import tune, register_env
 from src.envBuilder import buildFromArgs, createGymEnv, createUnityEnv
 
 
 @ray.remote
 class PPOTraining:
     
-    def __init__(self, args):
-        
-  
-        self.env = buildFromArgs(args)
-
-        self.args = args
+    def __init__(self, env, checkpoint_path, agent):
+        print(env)
+        self.env = createUnityEnv()
+        register_env("unity_env", createUnityEnv())
+        self.checkpoint_path = checkpoint_path
+        self.agent = agent
         # start environment
         # no graphics: faster, no visual rendering 
         #env = createUnityEnv(no_graphics=True)
         #env = gym.make('CartPole-v1')
 
-        state_dim = self.env.observation_space[0].shape[0]
+        #state_dim = self.env.observation_space[0].shape[0]
         
-        action_dim = self.env.action_space.shape[0]
+        #action_dim = self.env.action_space.shape[0]
     
-
+        ''' 
         # create directory and file to save checkpoint to
         directory = "PPO_preTrained"
         if not os.path.exists(directory):
@@ -36,10 +37,10 @@ class PPOTraining:
             os.makedirs(directory)
 
         self.checkpoint_path = os.path.join(directory, 'net_{}_{}'.format('logs', 0))
-        
+        '''
       
         # create PPO driven agent with hyperparameters
-        self.agent = PPO(state_dim, 
+        '''agent = PPO(state_dim, 
             action_dim, 
             args.lr_actor, 
             args.lr_critic, 
@@ -51,7 +52,7 @@ class PPOTraining:
 
 
         self.searchSpace = {
-            "env": self.env,
+            #"env": self.env,
             "agent": self.agent,
             "checkpoint_path": self.checkpoint_path,
             "render": True,
@@ -68,43 +69,47 @@ class PPOTraining:
             "save_model_freq": int(1e1)
         }
         
-    
-    def episode(env, checkpoint_path, agent,nr_episode=0, render=False, update_timestep=4000, action_std_decay_rate = 0.01, min_action_std = 0.001, action_std_decay_freq = int(2.5e5), save_model_freq = int(1e1)):
-        state = env.reset()
+    def getSearchSpace(self):
+        return self.searchSpace
+    '''
+    def episode(self, nr_episode=0, render=False, update_timestep=4000, action_std_decay_rate = 0.01, min_action_std = 0.001, action_std_decay_freq = int(2.5e5), save_model_freq = int(1e1)):
+       
+        state = self.env.reset()
         total_return = 0
         done = False
         time_step = 0
         print("episode:" + nr_episode)
         while not done:
             if render:
-                env.render()
+                self.env.render()
                     
             # 1. Select action according to policy
-            action = agent.select_action(state)
+            action = self.agent.select_action(state)
         
             # 2. Execute selected action
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, _ = self.env.step(action)
             
             # 3. Update buffer
-            agent.buffer.rewards.append(reward)         
-            agent.buffer.is_terminals.append(done)
+            self.agent.buffer.rewards.append(reward)         
+            self.agent.buffer.is_terminals.append(done)
 
-            # 4. Integrate new experience into agent
+            # 4. Integrate new experience into self.agent
             if time_step % update_timestep == 1:      
-                agent.update()
+                self.agent.update()
                 
                 
             if time_step % action_std_decay_freq == 1:
-                agent.decay_action_std(action_std_decay_rate, min_action_std)
+                self.agent.decay_action_std(action_std_decay_rate, min_action_std)
             
             state = next_state
             total_return += reward
             time_step += 1
 
             if time_step % save_model_freq == 0:
-                agent.save(checkpoint_path)
+                self.agent.save(self.checkpoint_path)
             
         print(nr_episode, ":", total_return)
+        self.env.close()
     
         return total_return
 
@@ -112,25 +117,29 @@ class PPOTraining:
     """
     at the moment without multiple instances at once
     """
-
+    
     def training(self, config):
-        
-        for nr_episode in range(config["nr_episodes"]):
-            self.episode(nr_episode, config["env"], config["agent"], config["checkpoint_path"], config["render"], config["update_timestep"], config["action_std_decay_rate"], config["min_action_std"], config["action_std_decay_freq"], config["save_model_freq"])
-  
-  
-    def startTraining(self, i):
+        print("env:")
+        print(self.env)
+        for nr_episode in range(1):
+            print(config)
+            self.episode(nr_episode, render=config["render"], update_timestep=config["update_timestep"], action_std_decay_rate=config["action_std_decay_rate"], min_action_std=config["min_action_std"], action_std_decay_freq=config["action_std_decay_freq"], save_model_freq=config["save_model_freq"])
             
-        print(i)
+            
+  
+    def startTraining(self, config, checkpoint_dir=None):
+                
+        
         # train agent with hyperparameter tuning
-        analysis = tune.run(self.training, config=self.searchSpace)
 
+        analysis = tune.run(self.training, config=config, num_samples= 1, resources_per_trial={"cpu": 2, "gpu": 1})
         
         # print ray tune analysis
+     
         print(analysis.get_best_config(metric="score", mode="min"))
-
+        
         #close environment
         #self.env.close()
         
-        return "test"
+        return analysis
         
