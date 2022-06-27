033d4d5ed5d2ae88d925e0c6e313fd54c079943b
Llewe <16879918+Llewe@users.noreply.github.com>
2022-06-27 16:50:54 +0200

    added basic slurm skript

---
+++ slurm_runner.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+python3 -m venv l-ki
+source l-ki/bin/activate
+
+pip3 install -r requirements.txt
+
+chmod -R 755 ./unity-env/*.x86_64
+
+python3 main.py -env unity -env_n 3DBall1 -tag slurm-test -e 10
