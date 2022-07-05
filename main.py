951f2fd093fc1583d909ae1cb793fc6f35b0540d
Llewe <16879918+Llewe@users.noreply.github.com>
2022-07-05 16:28:31 +0200

    changed episodes to timestep

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def parseArguments():
     parser.add_argument("-eval",   "--evaluation",  action="store_true",            help="run evaluation of trained model")
     parser.add_argument("-cpu",   "--force_cpu",    action="store_true",            help="forces to use the cpu")
     parser.add_argument("-agent",   "--agent",      default="ppo",  type=str,       help="set the agent type here. (ppo,random_agent,ppo-baseline)")
-    parser.add_argument("-tag", "--tag",            type=str, required=True, help="name/tag of the run")
+    parser.add_argument("-tag", "--tag",            type=str, required=True,        help="name/tag of the run")
     
     #hyperparameter
-    parser.add_argument("-e", "--episodes",                         default=1000,  type=int,           help="training episode number")
+    parser.add_argument("-e", "--episodes",                         default=1000,   type=int,           help="episode number")
+    parser.add_argument("-ts", "--max_timesteps",                   default=1200000,type=int,           help="number of timesteps in training")
     parser.add_argument("-us", "--update_timestep",                 default=1000,   type=int,           help="number of steps until update (n_steps/update_timestep) e.g. max_ep_len * 4")
     parser.add_argument("-g", "--gamma",                            default=0.99,   type=float,         help="discount factor, probably 0.99 at its best")
     parser.add_argument("-lr_a", "--lr_actor",                      default=1e-04,  type=float,         help="learn rate of the actor")
@@ -28,7 +29,7 @@ def parseArguments():
     parser.add_argument("-e_clip", "--epsilon_clip",                default=0.3,    type=float,         help="should probably be between [0.1, 0.3]")
     parser.add_argument("-a_std", "--action_std",                   default=0.5,    type=float,         help="")
     parser.add_argument("-a_std_rate", "--action_std_decay_rate",   default=5e-4,   type=float,         help="action standard deviation decay rate")
-    parser.add_argument("-a_std_freq", "--action_std_decay_freq",   default=1000,    type=int,           help="action standard deviation decay frequency")
+    parser.add_argument("-a_std_freq", "--action_std_decay_freq",   default=1000,   type=int,           help="action standard deviation decay frequency")
     parser.add_argument("-a_std_min", "--min_action_std",           default=1e-2,   type=float,         help="minimum action standard deviation")
     
     #env parameter
